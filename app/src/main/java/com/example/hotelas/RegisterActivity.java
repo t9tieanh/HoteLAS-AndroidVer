3d1747570ef62d11c5e5878fdf9261bc1c205ba2
t9tieanh <phama9162@gmail.com>
2025-04-27 14:41:03 +0700

    update login signup with gg/oauth2

--- app/src/main/java/com/example/hotelas/RegisterActivity.java
+++ app/src/main/java/com/example/hotelas/RegisterActivity.java
@@ -1,79 +1,125 @@
 package com.example.hotelas;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.hotelas.config.PrefManager;
 import com.example.hotelas.databinding.ActivityRegisterBinding;
-import com.google.firebase.auth.FirebaseAuth;
+import com.example.hotelas.model.response.ApiResponse;
+import com.example.hotelas.service.auth.AuthService;
+import com.example.hotelas.service.callback.ServiceExecutor;
+import com.example.hotelas.service.user.UserService;
+import com.example.hotelas.utils.ImageUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.MultipartBody;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
+    private UserService userService;
+    private PrefManager prefManager;
+
+    private String email;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Initialize binding
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Get email from intent
-        Intent intent = getIntent();
-        String email = intent.getStringExtra("email");
+        prefManager = new PrefManager(this);
+        String token = prefManager.getAuthResponse().getAccessToken();
+        userService = new UserService(token);
 
+        receiveIntentData();
+        setupListeners();
+    }
 
-        // Set click listener for the register button
+    private void setupListeners() {
         binding.createAndLoginButton.setOnClickListener(v -> {
-            if (email != null) {
-                createUser(email);
+            String username = binding.usernameEditText.getText().toString().trim();
+            String password = binding.passwordEditText.getText().toString();
+            String confirmPassword = binding.confirmPasswordEditText.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Vui lòng điền đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                return;
             }
+
+            if (!password.equals(confirmPassword)) {
+                Toast.makeText(this, "Mật khẩu không trùng khớp!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // thực hiện call
+            userService.activeAccount(RegisterActivity.this, username, password, name, email,
+                    ImageUtils.getBitmapFromImageView(binding.avatarImageView), new ServiceExecutor.CallBack<Boolean>() {
+                        @Override
+                        public void onSuccess(ApiResponse<Boolean> result) {
+                            if (result.getResult()) {
+                                Toast.makeText(RegisterActivity.this, "Đăng ký thành công cho " + username + "!", Toast.LENGTH_LONG).show();
+
+                                // xóa access token
+                                prefManager.clearAuthResponse();
+
+                                // chuyển về lại login
+                                Intent intent = new Intent(RegisterActivity.this, RegisterActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(String errorMessage) {
+                            Toast.makeText(RegisterActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+                    });
         });
     }
 
-    private void createUser(String email) {
-        String password = binding.passwordEditText.getText().toString();
-        String confirmPassword = binding.confirmPasswordEditText.getText().toString();
+    private void receiveIntentData() {
+        name = getIntent().getStringExtra("name");
+        email = getIntent().getStringExtra("email");
+        String imageUrl = getIntent().getStringExtra("imageUrl");
 
-        // Validate password and confirm password
-        if (TextUtils.isEmpty(password)) {
-            binding.passwordEditText.setError("Password cannot be empty");
-            binding.passwordEditText.requestFocus();
-            return;
-        } else if (TextUtils.isEmpty(confirmPassword)) {
-            binding.confirmPasswordEditText.setError("Confirm password cannot be empty");
-            binding.confirmPasswordEditText.requestFocus();
-            return;
-        } else if (password.length() < 8) {
-            binding.passwordEditText.setError("Password must be at least 8 characters long");
-            binding.passwordEditText.requestFocus();
-            return;
-        } else if (!password.matches(".*\\d.*")) {
-            binding.passwordEditText.setError("Password must contain at least one number");
-            binding.passwordEditText.requestFocus();
-            return;
-        } else if (!password.equals(confirmPassword)) {
-            binding.confirmPasswordEditText.setError("Passwords do not match");
-            binding.confirmPasswordEditText.requestFocus();
-            return;
+        if (name != null) {
+            binding.userNameDisplay.setText(name);
+            binding.createPasswordText.setText("Xin chào " + name + "! Nhập username và password để tiếp tục.");
         }
 
-        // Create user with email and password
-//        mAuth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, task -> {
-//                    if (task.isSuccessful()) {
-//                        // Registration success, user is created
-//                        Intent mainActivityIntent = new Intent(RegisterActivity.this, MainActivity.class);
-//                        startActivity(mainActivityIntent);
-//                        finish();
-//                    } else {
-//                        // Registration failed
-//                        Toast.makeText(RegisterActivity.this,
-//                                "Registration failed: " + task.getException().getMessage(),
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                });
+        if (imageUrl != null && !imageUrl.isEmpty()) {
+            // Nếu có hình ảnh, load hình bằng Glide
+            Glide.with(this)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.loading)  // Ảnh mặc định nếu chưa có
+                    .error(R.drawable.defaultprofileimage)        // Ảnh lỗi nếu load fail
+                    .centerCrop()
+                    .into(binding.avatarImageView);          // <-- Phải thay TextView avatar thành ImageView mới load được
+        }
     }
+
+//    private MultipartBody.Part getUserImage () {
+//        // Lấy ảnh từ ImageView
+//        Bitmap bitmap = ImageUtils.getBitmapFromImageView(binding.avatarImageView);
+//
+//        // Chuyển Bitmap thành File
+//        File imgFile = null;
+//        try {
+//            imgFile = ImageUtils.bitmapToFile(bitmap, getApplicationContext());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return ImageUtils.createImagePart(imgFile);
+//    }
+
 }
